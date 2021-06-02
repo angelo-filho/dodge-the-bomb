1bf545a107b15a4b95f825d8c0e9f232acbf3366
angelo-filho <amslf.snf20@uea.edu.br>
2021-06-01 23:49:29 -0400

    Update start_screen function

---
+++ screens/start_screen.py
@@ -0,0 +1,39 @@
+import sys
+
+import pygame
+from pygame.locals import *
+
+from os import path
+
+from screens.main_menu import main_menu
+
+snake_pong = pygame.image.load(path.join("assets", "sprites", "start_screen", "start_screen.png"))
+
+message = pygame.image.load(path.join("assets", "sprites", "start_screen", "start_screen_message.png"))
+
+
+def start_screen(screen):
+    clock = pygame.time.Clock()
+
+    message_alpha = 255
+    message_alpha_dir = -5
+    message.set_alpha(message_alpha)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == KEYDOWN:
+                main_menu(screen)
+
+        if (message_alpha >= 255 and message_alpha_dir > 0) or (message_alpha <= 0 and message_alpha_dir < 0):
+            message_alpha_dir *= -1
+        message_alpha += message_alpha_dir
+        message.set_alpha(message_alpha)
+
+        screen.fill((0, 0, 0))
+        screen.blit(snake_pong, (0, 0))
+        screen.blit(message, (155, 535))
+        pygame.display.flip()
+        clock.tick(60)
