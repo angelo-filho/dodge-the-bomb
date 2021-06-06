e3b1f2d072f070f41c73170499daa2c99d3aa9e5
angelo-filho <amslf.snf20@uea.edu.br>
2021-06-05 20:47:41 -0400

    Update constants.py

--- control/constants.py
+++ control/constants.py
@@ -2,10 +2,19 @@ import pygame
 
 from os.path import join
 
+# Window size
+WINDOW_SIZE = (680, 680)
+
 # Colors
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = "#656565"
 RED = "#FA8383"
+BLUE = "#355274"
+BLUE_LIGHT = "#83C1FA"
+YELLOW = "#F8FA83"
 
-#Fonts
+# Fonts
 pygame.font.init()
-small_font = pygame.font.Font(join("assets", "fonts", "VT323-Regular.ttf"), 42)
+SMALL_FONT = pygame.font.Font(join("assets", "fonts", "VT323-Regular.ttf"), 42)
+BIG_FONT = pygame.font.Font(join("assets", "fonts", "VT323-Regular.ttf"), 102)
