e7010452f33fc2d37886d84e09a4a8e8ce43bb70
angelo-filho <amslf.snf20@uea.edu.br>
2021-06-05 20:49:30 -0400

    Add SnakePiece.py

---
+++ entities/SnakePiece.py
@@ -0,0 +1,11 @@
+import pygame
+
+
+class SnakePiece:
+    def __init__(self, x, y, width, height):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.last = self.rect
+
+    def move(self, x, y):
+        self.rect.x += x
+        self.rect.y += y
